06f3d71d13faddea1f1733daa5d6364731aac097
Eitan Greene <Eitan-G@users.noreply.github.com>
2020-12-03 23:14:21 -0800

    Create Day-2.js

---
+++ Day-2.js
@@ -0,0 +1,34 @@
+const cheesyValidate = (string) => {
+  const { min, max, char, password } = getGroups(string)
+  const sortCharToStart = (a, b) => a === char ? -1 : 1
+  const count = password.split("").sort(sortCharToStart).lastIndexOf(char) + 1
+  return min <= count && max >= count
+}
+
+const lessCheesyValidate = (string) => {
+  const { min, max, char, password } = getGroups(string)
+  const count = password.match(new RegExp(char, "g"))?.length || 0
+  return min <= count && max >= count
+}
+
+const saneValidate = (string) => {
+  const { min, max, char, password } = getGroups(string)
+  let count = 0
+  for (const _char of password) {
+    if (_char === char) { count++ }
+    if (count > max) { return false }
+  }
+  return count >= min
+}
+
+const getGroups = string => {
+  const { groups } = string.match(/(?<min>\d+)-(?<max>\d+)\s(?<char>.):\s(?<password>.*)/)
+  return {
+    char: groups.char,
+    password: groups.password,
+    min: +groups.min,
+    max: +groups.max,
+  }
+}
+
+const countValidated = (passwords, validation) => passwords.filter(validation).length
